e4d45f9ecc892b0842c77f6e25189378c7d7ee76
a-sophie <a-sophie@yandex-team.ru>
2021-05-11 12:36:10 +0300

    add new generated files

---
+++ dist/module.js
@@ -0,0 +1,43 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";/************************************************************************/
+var __webpack_exports__ = {};
+var _ast = {
+
+  initialize: function() {
+    this._nodes = [];
+    this._node = {};
+    this._stash = [];
+  },
+
+  set: function(props) {
+    for (var k in props) this._node[k] = props[k];
+    return this._node;
+  },
+
+  node: function(obj) {
+    if (arguments.length) this._node = obj;
+    return this._node;
+  },
+
+  push: function() {
+    this._nodes.push(this._node);
+    this._node = {};
+  },
+
+  unshift: function() {
+    this._nodes.unshift(this._node);
+    this._node = {};
+  },
+
+  yield: function() {
+    var _nodes = this._nodes;
+    this.initialize();
+    return _nodes;
+  }
+};
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
